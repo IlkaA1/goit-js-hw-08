ebe4b25e25e74d536ea776119d1e9a0f0aafdb63
Ilona Kamash <ilonka.kamash@gmail.com>
2023-04-13 16:08:04 +0200

    виконане доопрацювання форми
    перевіряє форму на наявеісь всіх заповнених полів

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,8 +8,15 @@ form.addEventListener('submit', onSubmit);
 const formInput = {};
 
 
+
+
 function onSubmit(evt){
     evt.preventDefault();
+
+   if(form.elements.email.value === "" || form.elements.message.value === ""){
+    return console.log('Please, fill in all data 🧐')
+   }
+
     console.clear();
     evt.currentTarget.reset();
    localStorage.removeItem(LOCALSTORAGE_KEY);
@@ -18,15 +25,18 @@ function onSubmit(evt){
 
 function  handleInput(evt) {
     evt.preventDefault();
-
+   
     formInput[evt.target.name] = evt.target.value;
     
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formInput));
 };
 
 
+
+
+
 function populateSettings(){
-    const savedSettings = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)); 
+    const savedSettings = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
     if(savedSettings){
         const {email, message} = savedSettings;
          form.email.value = email;
